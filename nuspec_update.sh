33533afc542552aeb4b31df85b46c5c2f0365f57
lbrealdev <lbrealdeveloper@gmail.com>
2025-03-06 00:50:41 +0100

    fix: template.nuspec not found error

--- nuspec_update.sh
+++ nuspec_update.sh
@@ -12,6 +12,11 @@ fi
 INPUT_DIRECTORY="$1"
 INPUT_DIRECTORY=${INPUT_DIRECTORY%/}
 
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
+# Template file absolute path
+TEMPLATE_FILE="$SCRIPT_DIR/template.nuspec"
+
 function read_packages_config_file() {
   if [[ ! -f packages.config ]]; then
     echo "Error: packages.config file not found in $INPUT_DIRECTORY!"
@@ -72,13 +77,13 @@ if [[ -d "$INPUT_DIRECTORY" ]]; then
   echo "Reading packages.config..."
   read_packages_config_file
 
-  if [[ ! -f ../template.nuspec ]]; then
+  if [[ ! -f "$TEMPLATE_FILE" ]]; then
     echo "Error: Template template.nuspec not found!"
     exit 1
   fi
 
   echo "updating nuspec template..."
-  cp ../template.nuspec template.nuspec
+  cp "$TEMPLATE_FILE" template.nuspec
   update_template
 
   replace_template_tokens "MyFirstTestProjectNet48" "1.0.0"
